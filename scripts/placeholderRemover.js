5860a4df55ebf653c3ad952c5522b6b2e4e8a8d0
EtionPinari <etion.pinari@mail.polimi.it>
2021-04-04 11:47:36 +0200

    other page

--- scripts/placeholderRemover.js
+++ scripts/placeholderRemover.js
@@ -7,13 +7,28 @@ console.log(PicBG);
 /*
 INITIALIZE ALL BOO URL STRINGS TO USE AS THE ELEM SOURCE
 */
-var booDisney = "https://static.wikia.nocookie.net/disney/images/3/31/Profile_-_Boo.png/revision/latest/scale-to-width-down/633?cb=20190313094050"; 
+var booDisney = "https://images-na.ssl-images-amazon.com/images/I/51857ZP84DL._SX343_BO1,204,203,200_.jpg"; 
 var booDog = "https://i.pinimg.com/736x/24/5b/76/245b7634d8ac99a6a1e2edfda8e63fc2.jpg";
-var booSuperMario = "https://static.wikia.nocookie.net/nintendo/images/e/e4/MK8D_-_Boo_artwork.png/revision/latest?cb=20170113123150&path-prefix=en";
+var booSuperMario = "https://i.pinimg.com/564x/6f/57/99/6f57998a706f84d6c42032637e76ed05.jpg";
 var booScary = "https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/boo-scary-halloween-gift-for-a-halloween-ghost-tom-schiesswald.jpg";
 var booBats = "https://images-na.ssl-images-amazon.com/images/I/51vfl5nLQKL.jpg";
 
 var booList = [booDisney, booBats, booDog, booSuperMario, booScary];
-
-elem.setAttribute("src", booDisney);
+var index = getRandomIntInclusive(0,booList.length-1);
+elem.setAttribute("src", booList[index]);
+elem.setAttribute("width", 400);
+elem.setAttribute("height", 600);
 PicBG.appendChild(elem);
+
+
+
+
+
+
+
+				//FUNCTIONS//
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
